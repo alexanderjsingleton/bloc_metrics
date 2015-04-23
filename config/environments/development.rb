8d0c83179581ef9242d3d23c525e7fb3b97426ac
Alex Singleton <alexanderjsingleton@gmail.com>
2015-04-23 13:52:48 -0500

    changes to secrets.yml

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,5 +41,5 @@ Rails.application.configure do
 
    config.action_mailer.default_url_options = { host: 'localhost:3000' }
    config.action_mailer.delivery_method = :smtp
-   config.action_mailer.perform_deliveries = false
+   config.action_mailer.perform_deliveries = true
 end

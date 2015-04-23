6b2c17a4185782815adb572b3f588e4d5e3fea93
Alex Singleton <alexanderjsingleton@gmail.com>
2015-04-23 13:06:18 -0500

    for mentor review-sendgrid cred issues

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,4 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
-bloc_metrics.application.initialize!
-
-# config.action_mailer.default_url_options = { :host => 'localhost' }
+BlocMetrics::Application.initialize!
--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,3 +1,4 @@
+if Rails.env.development? || Rails.env.production?
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     address:        'smtp.sendgrid.net',
@@ -8,4 +9,4 @@
     domain:         'heroku.com',
     enable_starttls_auto: true
   }
-end
+end 

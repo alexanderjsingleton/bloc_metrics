44fbbbe2a323f6da37fb9f9e589621354578d601
Alex Singleton <alexanderjsingleton@gmail.com>
2015-04-29 15:38:53 -0500

    testing for schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,10 @@
-require 'faker'
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
  
 # Create an admin user
  admin = User.new(
@@ -9,59 +15,18 @@ require 'faker'
  admin.skip_confirmation!
  admin.save!
 
- # Create a moderator
- moderator = User.new(
-   name:     'Moderator User',
-   email:    'moderator@example.com',
-   password: 'helloworld',
-   role:     'moderator'
- )
- moderator.skip_confirmation!
- moderator.save!
+ #Create test user
+test = User.new(
+  email: 'test@example.com',
+  password: 'helloworld'
+  )
+
+test.skip_confirmation!
+test.save
  
- # Create a member
- member = User.new(
-   name:     'Member User',
-   email:    'member@example.com',
-   password: 'helloworld'
- )
- member.skip_confirmation!
- member.save!
-
-
-
-# Create Users
-5.times do
-  user = User.new(
-    # name:     Faker::Name.name,
-    email:    Faker::Internet.email,
-    password: Faker::Lorem.characters(10)
-  )
-  user.skip_confirmation!
-  user.save!
+ 5.times do
+  registered_application = Application.create!(
+    user: test,
+    url: Faker::Internet.url
+    )
 end
-users = User.all
-
-
-# Create Applications
-10.times 
- Application.create!(
-  name:         Faker::Name.name, 
-   url:         Faker::Internet.url 
- )
-end
-applications = Application.all
-
-#Create Events
- 50.times do
-  Event.create!(
-    user:   users.sample,
-    application:  Applications.sample,
-    name:  Events.sample
-  )
-end
-event = Events.all
-
-puts "Seed finished"
-puts "#{Event.count} events created"
-puts "#{Application.count} applications created"

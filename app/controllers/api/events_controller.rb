ab1a3a162e9748cd5d326644d22de163a9bee162
Alex Singleton <alexanderjsingleton@gmail.com>
2015-04-30 10:30:18 -0500

    create algo for api/events_contoller

---
+++ app/controllers/api/events_controller.rb
@@ -0,0 +1,27 @@
+ class API::EventsController < ApplicationController
+# #3
+   skip_before_action :verify_authenticity_token
+ 
+   def create
+     registered_application = RegisteredApplication.find_by(url: request.env['HTTP_ORIGIN'])
+      if registered_application.nil?
+      render json: "Unregistered application", status: :unprocessable_entity
+      elsif @event = registered_application.events.build(event_params)
+         @event.save
+         render json: @event, status: :created
+      else
+        render @event.errors, status: :unprocessable_entity
+      end
+    end
+   end
+
+   private
+   
+   def event_params
+     params.require(:event).permit(:name)
+   end
+
+ end
+
+ # curl command for API
+ # curl -v -H "Accept: application/json" -H "Origin: http://registered_application.com" -H "Content-Type: application/json" -X POST -d '{"event_name":"foobar"}'  http://localhost:3000/api/events
